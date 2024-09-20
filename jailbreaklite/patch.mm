2f6b31302578447d345f513f68707351ff0ff754
Sylphin <130824149+Sylphin3107@users.noreply.github.com>
2024-09-20 19:17:27 +0700

    Add files via upload

---
+++ jailbreaklite/patch.mm
@@ -0,0 +1,33 @@
+#import "sylphin3107.h"
+#import "mahoa.h"
+@interface Menu : NSObject
+@end
+@implementation Menu
++(void)load{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        anogs("0x9AD1C", "0xC0035FD6");
+        anogs("0x132974", "0xC0035FD6");
+
+        unity("0x52439D8", "0xC0035FD6");
+        unity("0x5243AD8", "0xC0035FD6");
+        unity("0x524C19C", "0xC0035FD6");
+        unity("0x5331A08", "0x360080D2");
+        unity("0x6132198", "0x0110201E");
+        unity("0x61321A4", "0x0190201E");
+        unity("0x5E21A50", "0x370080D2");
+        unity("0x5C25994", "0x000080D2");
+        unity("0x5C25998", "0xC0035FD6");
+        unity("0x5C4C590", "0x200080D2");
+        unity("0x5C4C594", "0xC0035FD6");
+        unity("0x5FC0FF8", "0x1F2003D5");
+        unity("0x5E17B98", "0x1F2003D5");
+        unity("0x5E17C28", "0x330080D2");
+        unity("0x574075C", "0x330080D2");
+        unity("0x57405F8", "0x330080D2");
+        unity("0x5EEC550", "0x1F2003D5");
+        unity("0x5E46F50", "0xC0035FD6");
+        unity("0x5E3AD60", "0x200080D2");
+        unity("0x5E3AD64", "0xC0035FD6");
+    });
+}
+@end
